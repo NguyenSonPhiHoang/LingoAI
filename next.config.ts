9f4812f99b82a0c51aec922bfb57f91e2d087703
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-08-19 01:02:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push('async_hooks');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
