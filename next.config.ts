1e0592fdff0cca15fd1d4a48210c78e31479fb30
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-08-19 01:08:57 +0000

    Module not found: Can't resolve 'async_hooks' bị lỗi này

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@opentelemetry/api'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
