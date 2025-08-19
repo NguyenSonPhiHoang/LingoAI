72092f2593ddb937c02ba76f47f1232f2597e1c4
Nguyễn Minh Phúc <phuc@lhu.edu.vn>
2025-08-19 01:05:34 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -19,12 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push('async_hooks');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
