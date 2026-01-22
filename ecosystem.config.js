069f6eedb17891aa376ca90dfc9b26cd9b79db91
testus-jpg <nyb43326@doolk.com>
2026-01-22 15:28:55 +0700

    update pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,11 @@ module.exports = {
     apps: [
         {
             name: 'lingoAI',
-            cwd: '/home/fitlhu-lingoai/htdocs/lingoai.fitlhu.com/lingoAI',
+            cwd: './',
             script: 'node_modules/next/dist/bin/next',
-            args: 'start -p 3032',
+            args: 'start -p 3124',
             env: {
-                NODE_ENV: 'production',
-                env: {
-                    VERTEXAI_PROJECT: "your-project-id",
-                    GOOGLE_CLOUD_PROJECT: "your-project-id",
-                    VERTEXAI_LOCATION: "us-central1",
-                    GOOGLE_APPLICATION_CREDENTIALS: "/opt/keys/svc-vertex.json"
-                }
+                NODE_ENV: 'production'
             }
         }
     ]
